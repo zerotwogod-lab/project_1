c3b510311f7174ca54dfbe19c742e63d8a81fa09
anton <zerotwogod@icloud.com>
2026-05-03 03:19:49 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from application.salary import calculate_salary
-from application.db.people import get_employees
-
-if __name__ == '__main__':
-    print("Текущая дата:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    calculate_salary()
-    get_employees()
